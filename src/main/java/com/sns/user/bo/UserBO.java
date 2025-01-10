611701ad5d6b538c65ad27c0f25860e0bade6717
DESKTOP-HME3UEB\403megait <test@test.com>
2025-01-10 18:40:37 +0900

    SNS 회원가입 완료

---
+++ src/main/java/com/sns/user/bo/UserBO.java
@@ -0,0 +1,40 @@
+package com.sns.user.bo;
+
+import com.sns.common.EncryptUtils;
+import com.sns.user.entity.UserEntity;
+import com.sns.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserBO {
+    @Autowired
+    private UserRepository userRepository;
+
+    // i: loginId
+    // o: UserEntity(단건) or null
+    public UserEntity getUserEntityByLoginId(String loginId) {
+        return userRepository.findByLoginId(loginId).orElse(null);
+    }
+
+    // i: 4개 파라미터
+    // o: UserEntity => boolean(true:성공)
+    public boolean addUser(String loginId, String password, String name, String email) {
+        // md5 알고리즘 - hashing
+        // aaaa -> 74b8733745420d4d33f80c4663dc5e5
+        // aaaa -> 74b8733745420d4d33f80c4663dc5e5
+        String hashedPassword = EncryptUtils.md5(password);
+
+        // 실질적으로 오류 발생 시 null return 되지 않음. (try-catch로 처리해야함)
+        UserEntity user = userRepository.save(
+                UserEntity.builder()
+                        .loginId(loginId)
+                        .password(hashedPassword)
+                        .name(name)
+                        .email(email)
+                        .build()
+        );
+
+        return user == null ? false : true;
+    }
+}
