d8f2a5cf68bb49811519cd6275e2136235c7b06f
DESKTOP-HME3UEB\403megait <test@test.com>
2025-01-14 17:51:29 +0900

    타임라인 글 목록 뿌리기

---
+++ src/main/java/com/sns/post/entity/PostEntity.java
@@ -0,0 +1,37 @@
+package com.sns.post.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Table(name = "post")
+@Entity
+public class PostEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "userId")
+    private int userId;
+
+    private String content;
+
+    @Column(name = "imagePath")
+    private String imagePath;
+
+    @CreationTimestamp
+    @Column(name = "createdAt")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updatedAt")
+    private LocalDateTime updatedAt;
+}
--- src/main/java/com/sns/timeline/TimelineController.java
+++ src/main/java/com/sns/timeline/TimelineController.java
@@ -1,13 +1,24 @@
 package com.sns.timeline;
 
+import com.sns.post.bo.PostBO;
+import com.sns.post.entity.PostEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class TimelineController {
+    private final PostBO postBO;
 
     @GetMapping("/timeline")
-    public String timeline() {
+    public String timeline(Model model) {
+        List<PostEntity> postList = postBO.getPostList();
+        model.addAttribute("postList", postList);
+
         return "timeline/timeline";
     }
 }
